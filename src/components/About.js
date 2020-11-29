baf6ffeebc9d1dce2410a3cccbb668f418a6fca7
Ubaid Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-11-29 18:30:35 +0530

    Route

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = () => (
+  <div>
+    <h1>You are on the about page.</h1>
+    <a href="./">Home</a>
+  </div>
+);
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,29 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 class App extends Component {
-    render() {
+  render() {
+    return (
+      <div id="main">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Redirect to="/" />
+        </Switch>
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+        {/* Do not remove the main div */}
+      </div>
+    );
+  }
 }
-
 
 export default App;
