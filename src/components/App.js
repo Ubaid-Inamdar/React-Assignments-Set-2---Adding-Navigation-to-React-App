8c9cc6c8a4c41118a24150121b713093c155d67a
Ubaid Inamdar <68742258+Ubaid-Inamdar@users.noreply.github.com>
2020-12-01 21:03:18 +0530

    route

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,77 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import Home from "./Home";
-import About from "./About";
-import Error from "./Error";
-import {
-  BrowserRouter as Link,
-  Switch,
-  Route,
-  Redirect,
-  useLocation
-} from "react-router-dom";
-function LocationDisplayComponent() {
-  let location = useLocation();
-  return <div data-testid="location-display">{location.pathname}</div>;
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import LocationDisplayComponent from "./LocationDisplayComponent";
+
+function About() {
+  return <div>You are on the about page.</div>;
+}
+function Home() {
+  return <div>You are home.</div>;
+}
+
+function Invalid() {
+  return <div>No match</div>;
 }
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/" component={Error} />
-        </Switch>
-        <LocationDisplayComponent />
-
-        {/* Do not remove the main div */}
-      </div>
+      <>
+        <div id="main">
+          {/* Do not remove the main div */}
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Switch>
+            <Route path="/about" component={About} />
+            <Route path="/" exact component={Home} />
+            <Route path="/" component={Invalid} />
+          </Switch>
+          <LocationDisplayComponent />
+        </div>
+      </>
     );
   }
 }
 
 export default App;
 export const LocationDisplay = LocationDisplayComponent;
+
+// import React, { Component, useState } from "react";
+// import "../styles/App.css";
+// import Home from "./Home";
+// import About from "./About";
+// import Error from "./Error";
+// import {
+//   BrowserRouter as Link,
+//   Switch,
+//   Route,
+//   Redirect,
+//   useLocation
+// } from "react-router-dom";
+// function LocationDisplayComponent() {
+//   let location = useLocation();
+//   return <div data-testid="location-display">{location.pathname}</div>;
+// }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div id="main">
+//         <a href="/">Home</a>
+//         <a href="/about">About</a>
+//         <Switch>
+//           <Route path="/" exact component={Home} />
+//           <Route path="/about" component={About} />
+//           <Route path="/" component={Error} />
+//         </Switch>
+//         <LocationDisplayComponent />
+
+//         {/* Do not remove the main div */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+// export const LocationDisplay = LocationDisplayComponent;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import React from "react";
 const Home = () => (
   <div>
     <h1>You are home.</h1>
-    <p>You are at default Home Page</p>
   </div>
 );
 
